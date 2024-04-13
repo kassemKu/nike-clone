a2dd9ddf412c33b7ba88bb812c0a33f9439c5cdf
kassemKu <arabisc95@gmail.com>
2024-04-13 16:05:33 +0300

    update: clean code up

--- app/Components/Nav.tsx
+++ app/Components/Nav.tsx
@@ -1,5 +1,3 @@
-import { headerLogo } from "public/images";
-import { hamburger } from "public/icons";
 import { Link } from "@remix-run/react";
 import { navLinks } from "../Constants";
 
@@ -8,7 +6,12 @@ const Nav = () => {
     <header className="sm:px-16 px-8 py-8 absolute z-10 w-full">
       <nav className="flex justify-between items-center max-w-[1440px] my-0 mx-auto">
         <Link to="/">
-          <img src={headerLogo} alt="Logo" width={130} height={29} />
+          <img
+            src={"/images/header-logo.svg"}
+            alt="Logo"
+            width={130}
+            height={29}
+          />
         </Link>
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
           {navLinks.map((item) => (
@@ -23,7 +26,12 @@ const Nav = () => {
           ))}
         </ul>
         <div className="hidden max-lg:block">
-          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+          <img
+            src={"/icons/hamburger.svg"}
+            alt="Hamburger"
+            width={25}
+            height={25}
+          />
         </div>
       </nav>
     </header>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,4 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
 import {
   CustomerReviews,
   Footer,
